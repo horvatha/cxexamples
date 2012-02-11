71ac8f258c9bb4a459c26f9752be7eefa25153a7
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2012-02-12 00:01:54 +0100

    new file:   prime.py first version without assert

---
+++ prime/prime.py
@@ -0,0 +1,38 @@
+##!/usr/bin/env python
+# coding: utf-8
+
+"""Lists primes and twin primes.
+"""
+
+from __future__ import division
+from __future__ import print_function
+
+__author__ = 'Arpad Horvath'
+# cl wh for ifmain def defs imp
+
+class Primes(object):
+    """Create the list of primes.
+    """
+    def __init__(self, maxi=10000):
+        self.maxi = maxi
+        nums = range(maxi+1)
+        nums[0:2] = [0, 0]
+        for i in range(2, maxi+1):
+            if nums[i] != 0:
+                j = 2*i
+                while j <= maxi:
+                    nums[j] = 0
+                    j += i
+        self.primes = [p for p in nums if p != 0]
+
+    def twin_primes(self):
+        return [(self.primes[i], self.primes[i+1])
+                for i in range(len(self.primes) - 1)
+                if self.primes[i] + 2 == self.primes[i+1]
+            ]
+
+    def isprime(self, n):
+        return n in self.primes
+
+
+
