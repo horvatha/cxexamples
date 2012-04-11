866a7c37061d0eb6228bc416c755f0798269691a
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2012-04-11 09:01:24 +0200

    pavprufer.py Új beadandó eredeti változat (átnevezve)

---
+++ bead/pavprufer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Docstring of the module beadando
+"""
+
+from __future__ import division
+from __future__ import print_function
+import igraph
+import sys
+
+__author__ = 'Paveszka Sándor'
+# wh for def cl defs ifmain imp fr _ pdb + <Tab>
+class Network(igraph.Graph):
+    def coode(self):
+        li = []
+        vc = self.vcount()
+        ec = self.ecount()
+        if ec == vc -1 and self.is_connected():
+            while vc > 2:
+                level = self.degree().index(1)
+                neig = self.neighbors(level)[0]
+                name = int(self.vs[neig]["name"])
+                li.append(name)
+                self.delete_vertices(level)
+            return li
+        else:
+            return None
+
+def test():
+    for  hdill in ["1--4--7, 2--5--8, 3--2--6",
+            "1--2--3--4, 2--5, 3--6"]:
+        testnet = Network.Formula(hdill)
+        net = testnet.copy()
+        code = testnet.coode()
+        if code is None:
+            print ("None")
+        else:
+            print (format(code))
+
+if __name__ == "__main__":
+    test()
